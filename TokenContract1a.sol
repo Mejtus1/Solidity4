a00d18a12052179cef847970121be82777378974
Mejtus1 <GitMaster1769@proton.me>
2023-12-07 12:33:29 +0100

    Token1 crresponding transactions

---
+++ TokenContract1a.sol
@@ -0,0 +1,54 @@
+// This file explains etherscan transactions related to code
+
+// Token Contract Creator address 
+// https://etherscan.io/address/0x655cfe8e28f86b9738fe04fb76c19b54bdbd46f
+// Token Contract 
+// https://etherscan.io/token/0x655cfe8e28f86b9738fe04fb76c19b54bdbd46f7
+
+// we have here on ETHERSCAN 
+// CREATOR WALLET https://etherscan.io/address/0x3d05ac7a05710d41f67c5a13d624bc3ab8c83874
+// CONTRACT https://etherscan.io/address/0x655cfe8e28f86b9738fe04fb76c19b54bdbd46f7
+// TOKEN https://etherscan.io/token/0x655cfe8e28f86b9738fe04fb76c19b54bdbd46f7
+
+
+
+// 1. wallet created token 0x655cfe8e28f86b9738fe04fb76c19b54bdbd46f7 (TOKEN CREATOR)
+// 1. send ether, renounced ownership added liquidity
+0x5d5887c2aca02b741c9e2bd45541f0610c809a871ee1219961fe35c6c037b8a9 Transfer     18205648 72 days 57 min ago 0x3d05Ac...b8c83874 OUT 0x6F0bfd...9C6d64e5 1   ETH	0.00015207
+0x47328269a3a68d7b9f236dda4abd436ae1f36f848e756ac9917fb51dd4e2cfd3 Transfer     18205647 72 days 57 min ago 0x3d05Ac...b8c83874 OUT 0xB3DCC1...18685A26 0.1 ETH	0.00015014
+0x7087c254e007f6be5989dc75475109dbdfb39b01d325bde7292719358500ed53 Transfer     18205646 72 days 58 minsago 0x3d05Ac...b8c83874 OUT 0xD981dC...93b647d0 0.1 ETH	0.00015077
+0xbc25d114d52904013cfd5698221f6a40e9c77bdbd7669e70c20109fe21847b48 Transfer     18194973 73 days 12 hrs ago 0x3d05Ac...b8c83874 OUT 0x03ffE4...8A3AdF44 0.2 ETH	0.0001565
+0x8db171b42c698fa06dde361b71f2f23bda4f717b3d648be44f4342cb4432dea2 Ren. Own     18194577 73 days 14 hrs ago	0x3d05Ac...b8c83874 OUT 0x655cFe...bDbd46F7 0   ETH	0.00018418
+0x54968b37b163325c08bd9f85935e4102e6f8259b33b35c95d55a7df9fdad6572 Lock LP      18194563 73 days 14 hrs ago	0x3d05Ac...b8c83874 OUT UNCX Network Security : LP Lockers 0.1 ETH	0.00304983
+0xb35ddc679bb8f071bf1061a5cc7f13e7f190fa8672e95a99ec872d066fd79615 Approve      18194559 73 days 14 hrs ago	0x3d05Ac...b8c83874 OUT Uniswap V2: $FC24 0 ETH	0.00036442
+0x43e23f60d4ec34f13737b16bf1f067128f6dc7f09b301ba105b0450938f5c8fe Transfer     18194554 73 days 14 hrs ago	0x3d05Ac...b8c83874 OUT 0x654835...62d91D3C 0.18 ETH	0.00016756
+0x90841810e69ed31dcb721123de88eb3a8258ad7eed3a253e6ba19d01ee610acd Open Trading 18194547 73 days 14 hrs ago	0x3d05Ac...b8c83874 OUT 0x655cFe...bDbd46F7 0 ETH	0.0214023
+0xb2b59bd63533dc67d8f10658f323fc3914dd58cfa6542bf6d9db1bd83afe1c01 Transfer     18194540 73 days 14 hrs ago	0x3d05Ac...b8c83874 OUT	0x655cFe...bDbd46F7 1 ETH	0.00015617
+0xcd75c9dfb8f95a375f8ee9acdeb589ae8edab474c71cf9dad0d722684240f278 Transfer     18194539 73 days 14 hrs ago	0x3d05Ac...b8c83874 OUT	0x655cFe...bDbd46F7 0 ETH	0.00043744
+0x1c296f32f4ca0514874c9699f2f151f2d9773bf67187e08f1f6ba9057bbed28e Transfer     18190876 74 days 12 hrs ago	0x3d05Ac...b8c83874 OUT	0x8C0eA7...0c59A208 0.03 ETH	0.00016955
+0xdd1a584868180044153c555b8943e78ded8c257dc1b86c9122e4a18fecda39e4 0x60806040   18187821 74 days 12 hrs ago	0x3d05Ac...b8c83874 OUT	Create: FC24        0 ETH	0.02675407
+0x68c5c9bf585982bce867f6c33ca237ce56194f228683d503259d7d82a8eadac8 0x60806040   18187638 74 days 13 hrs ago	0x3d05Ac...b8c83874 OUT	Create: FC24        0 ETH	0.02833115
+0xb877a0bea0538def777d9bd82e4d98371b9b348995b270e476ed65f8f137a760 Transfer     18173534 76 days 12 hrs ago	FixedFloat          IN	0x3d05Ac...b8c83874 1.4870475 ETH	0.000273
+// ^ starts with this transaction 
+
+// 1. Internal transactions for TOKEN CREATOR WALLET
+0x431a5174533ba5dc31404c09e2b969fe2228f13cda952efd18cf3c20f7c5da25 18194576 73 days 23 hrs ago 0x655cFe...bDbd46F7 0x3d05Ac...b8c83874 0.07959586 ETH
+0x43aebd713479a6b664f0549ac85cf8d3fa67181cbe9b053ccb3a7a06ca88977b 18194576 73 days 23 hrs ago 0x655cFe...bDbd46F7 0x3d05Ac...b8c83874 0.06595406 ETH
+0xf212dec78ff2ed709e40f5d0044722c949e21cf41d4fb157c9c921abb4c113b2 18194558 74 days 2 mins ago 0x655cFe...bDbd46F7 0x3d05Ac...b8c83874 0.06065744 ETH
+
+// internal transaction example explained
+// 
+From: 0xfB2660cb9aDe9828C4D94AbACC19521484BDb76E Interacted With (To): 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D (Uniswap V2: Router 2)
+Transfer 0.079595867785985322 ETH From Wrapped Ether        To Uniswap V2: Router 2 // transfer of 0.079 eth to uniswap
+Transfer 0.079595867785985322 ETH From Uniswap V2: Router 2 To 0x655cFe...bDbd46F7  // transfer from uniswap to contract
+Transfer 0.079595867785985322 ETH From 0x655cFe...bDbd46F7  To 0x3d05Ac...b8c83874  // transfer buy fees from contract to TOKEN CREATOR ADDRESS
+Transfer 0.073524444640519046 ETH From Wrapped Ether        To Uniswap V2: Router 2 // ? 
+Transfer 0.073524444640519046 ETH From Uniswap V2: Router 2 To 0xfB2660...84BDb76E // ? 
+
+// 2. Token contract 0x655cfe8e28f86b9738fe04fb76c19b54bdbd46f7
+// this is token contract and transactions correspont with upper ones, after open trading the trading starts and people buy in 
+0x90841810e69ed31dcb721123de88eb3a8258ad7eed3a253e6ba19d01ee610acd Open Trading 73 days 14 hrs ago 0x655cFe...bDbd46F7 Uniswap V2: $FC24    76,000,000
+0x90841810e69ed31dcb721123de88eb3a8258ad7eed3a253e6ba19d01ee610acd Open Trading 73 days 13 hrs ago 0x655cFe...bDbd46F7 0x655cFe...bDbd46F7  19,000,000
+0xcd75c9dfb8f95a375f8ee9acdeb589ae8edab474c71cf9dad0d722684240f278 Transfer     73 days 13 hrs ago 0x3d05Ac...b8c83874 0x655cFe...bDbd46F7  95,000,000
+0xdd1a584868180044153c555b8943e78ded8c257dc1b86c9122e4a18fecda39e4 0x60806040   74 days 13 hrs ago Null: 0x000...000   0x3d05Ac...b8c83874  100,000,000
+// ^ starts with this transaction 
